59b4e71c0235a25e5f4c41a48ea7eaabf73ecc59
Muhammad Falak R Wani <falakreyaz@gmail.com>
2017-09-09 08:45:28 +0530

    golang-intro: add content for vals and vars

---
+++ golang-intro/src/val_var.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println(true && false)
+
+	var a string = "initial"
+	fmt.Println(a)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "short cut for variable declaration" // HL
+	fmt.Println(f)
+}
