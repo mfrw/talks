96b529ac52bdde9610a33549b0e2942b3bc64677
Muhammad Falak R Wani <falakreyaz@gmail.com>
2017-09-15 15:16:40 +0530

    golang-intro: add arrays and slices

---
+++ golang-intro/src/array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2D: ", twoD)
+}
